e6ca3265ff01f1b7171c683c37c87d633ff624d9
Chris Shen <chrissh1124@gmail.com>
2021-04-14 23:17:14 -0500

    new app

---
+++ official/User/forms.py
@@ -0,0 +1,33 @@
+from django.forms import ModelForm
+from django.forms import Form
+from django import forms
+from .models import Enrollment
+from .models import Courses
+from .models import LoginInfo
+from .models import Students
+
+class EnrollmentForm(ModelForm):
+    class Meta:
+        model = Enrollment
+        fields = ['NetId', 'CRN']
+
+class CoursesForm(ModelForm):
+    class Meta:
+        model = Courses
+        fields = ['CRN', 'CourseTitle', 'CourseNumber', 'Department', 'Section', 'ScheduleType', 'Instructor', 'MeetingTime']
+
+class LoginInfoForm(ModelForm):
+    class Meta:
+        model = LoginInfo
+        fields = ['AccountName', 'Password']
+
+class StudentsForm(ModelForm):
+    class Meta:
+        model = Students
+        fields = ['NetId', 'FirstName', 'LastName', 'Preferred_Work_Time', 'SchoolYear', 'ContactInfo', 'OtherInfo']
+
+class Query3Form(Form):
+    netid = forms.CharField(max_length=100)
+
+class Query2Form(Form):
+    netid = forms.CharField(max_length=100)
